3770211fd20405fa8c3ef843e2d3a890e96a7268
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-01-29 21:27:40 +0200

    fix error

--- src/components/RestrictedRoute/RestrictedRoute.tsx
+++ src/components/RestrictedRoute/RestrictedRoute.tsx
@@ -1,9 +1,9 @@
-import { FC } from 'react';
-import { IProps } from './RestrictedRoute.types';
+// import { FC } from 'react';
+// import { IProps } from './RestrictedRoute.types';
 
-export const RestrictedRoute: FC<IProps> = ({
-  component: Component,
-  redirectTo = '/',
-}) => {
-  return Component;
-};
+// export const RestrictedRoute: FC<IProps> = ({
+//   component: Component,
+//   redirectTo = '/',
+// }) => {
+//   return Component;
+// };
