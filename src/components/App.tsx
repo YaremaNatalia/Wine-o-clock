a9ef6a5e2a4a3809ad13e0dd7c38da9cef2831c9
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-01-31 09:57:04 +0200

    add index.ts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
-import Container from './Container/Container';
-import Section from './Section/Section';
+import Container from '@/components/Container';
+import Section from '@/components/Section';
 
 const App = () => {
   return (
