b62e7c6571d5b1ff4e99afd8e108d9b55faeb802
YaremaNatalia <yaremka.n@gmail.com>
2024-02-09 05:02:24 +0200

    add animation

--- src/components/MainHero/MainHero.styled.ts
+++ src/components/MainHero/MainHero.styled.ts
@@ -8,23 +8,186 @@ export const MainHeroStyled = styled.div`
   justify-content: center;
   flex-direction: column;
 
-  /* @media screen and (min-width: 768px) {
-    flex-direction: row;
-    gap: ${({ theme }) => theme.spacing(5)};
+  & > div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    & > p {
+      margin-bottom: ${({ theme }) => theme.spacing(3)};
+      font-size: 14px;
+      font-weight: ${({ theme }) => theme.fontWeight.regular};
+
+      @media screen and (min-width: 768px) {
+        font-size: 20px;
+      }
+
+      @media screen and (min-width: 1440px) {
+        margin-bottom: ${({ theme }) => theme.spacing(15)};
+        margin-right: auto;
+        width: ${({ theme }) => theme.spacing(157)};
+      }
+    }
+
+    & > a {
+      margin-right: auto;
+
+      & > button {
+        margin-bottom: ${({ theme }) => theme.spacing(15)};
+
+        @media screen and (min-width: 768px) {
+          margin-bottom: ${({ theme }) => theme.spacing(7)};
+          width: 245px;
+          height: 48px;
+        }
+        @media screen and (min-width: 1440px) {
+          margin-bottom: ${({ theme }) => theme.spacing(12)};
+        }
+      }
+    }
+  }
+`;
+
+export const HeroTextWrapper = styled.div`
+  display: flex;
+  align-items: start;
+  justify-content: center;
+  flex-direction: column;
+  margin-bottom: ${({ theme }) => theme.spacing(8)};
+  padding-left: ${({ theme }) => theme.spacing(9)};
+
+  @media screen and (min-width: 768px) {
+    margin-right: auto;
+    margin-bottom: ${({ theme }) => theme.spacing(4)};
+    padding-left: 0;
   }
 
   @media screen and (min-width: 1440px) {
-    gap: ${({ theme }) => theme.spacing(28)};
-  } */
-`;
-export const HeroTextWrapper = styled.div`
-  h1 {
+    margin-bottom: ${({ theme }) => theme.spacing(12)};
+  }
+
+  & > h1 {
     text-transform: uppercase;
     font-family: ${({ theme }) => theme.fontFamily.otherFontFamily};
     font-size: 32px;
     font-weight: ${({ theme }) => theme.fontWeight.bold};
+
+    @media screen and (min-width: 768px) {
+      font-size: 48px;
+    }
+
+    @media screen and (min-width: 1440px) {
+      font-size: 76px;
+    }
+
+    & > span {
+      color: ${({ theme }) => theme.colors.primaryBurgundy};
+    }
+  }
+
+  & > .firstHeroLine {
+    @media screen and (min-width: 768px) {
+      padding-left: ${({ theme }) => theme.spacing(17)};
+    }
+
+    @media screen and (min-width: 1440px) {
+      padding-left: ${({ theme }) => theme.spacing(26)};
+    }
+  }
+
+  & > .thirdHeroLine {
+    @media screen and (min-width: 768px) {
+      padding-left: ${({ theme }) => theme.spacing(26)};
+    }
   }
 `;
-export const HeroPictureWrapper = styled.div``;
-export const HeroBg = styled.img``;
-export const SvgWrapper = styled.div``;
+
+export const HeroPictureWrapper = styled.div`
+  position: relative;
+
+  svg {
+    @media screen and (min-width: 768px) {
+      width: 56px;
+      height: 37px;
+    }
+
+    @media screen and (min-width: 1440px) {
+      width: 72px;
+      height: 47px;
+    }
+  }
+
+  > div {
+    position: absolute;
+    display: flex;
+    flex-direction: row;
+    gap: ${({ theme }) => theme.spacing(3)};
+    right: ${({ theme }) => theme.spacing(6)};
+
+    @media screen and (min-width: 768px) {
+      right: ${({ theme }) => theme.spacing(10)};
+      gap: ${({ theme }) => theme.spacing(4)};
+    }
+
+    @media screen and (min-width: 1440px) {
+      right: ${({ theme }) => theme.spacing(20)};
+    }
+  }
+`;
+
+export const SvgTopLine = styled.div`
+  top: ${({ theme }) => theme.spacing(-8)};
+
+  @media screen and (min-width: 768px) {
+    top: ${({ theme }) => theme.spacing(-12)};
+  }
+
+  @media screen and (min-width: 1440px) {
+    top: ${({ theme }) => theme.spacing(-16)};
+  }
+
+  svg {
+    fill: ${({ theme }) => theme.colors.primaryBlack};
+  }
+
+  animation: slideFromLeft 3s ease forwards;
+  @keyframes slideFromLeft {
+    from {
+      transform: translateX(-100%);
+      opacity: 0;
+    }
+    to {
+      transform: translateX(0);
+      opacity: 1;
+    }
+  }
+`;
+
+export const SvgBottomLine = styled.div`
+  top: ${({ theme }) => theme.spacing(2)};
+
+  @media screen and (min-width: 768px) {
+    top: ${({ theme }) => theme.spacing(3)};
+  }
+
+  @media screen and (min-width: 1440px) {
+    top: ${({ theme }) => theme.spacing(6)};
+  }
+
+  svg {
+    fill: ${({ theme }) => theme.colors.primaryWhite};
+  }
+
+  animation: slideFromRight 3s ease forwards;
+  @keyframes slideFromRight {
+    from {
+      transform: translateX(100%);
+      opacity: 0;
+    }
+    to {
+      transform: translateX(0);
+      opacity: 1;
+    }
+  }
+`;
--- src/components/MainHero/MainHero.tsx
+++ src/components/MainHero/MainHero.tsx
@@ -1,16 +1,29 @@
 import { FC } from 'react';
-import { ButtonDesign, ButtonTypes } from '@/constants';
+import { Link } from 'react-router-dom';
+import Container from '@/components/Container';
+import Section from '@/components/Section';
+import { ButtonTypes } from '@/constants';
 import Button from '@/components/Button';
-import { wineTime } from '@/images/mainPage';
+import { hero } from '@/images/mainPage';
+import W from '@/icons/wIcon.svg?react';
+import E from '@/icons/eIcon.svg?react';
+import L from '@/icons/lIcon.svg?react';
+import O from '@/icons/oIcon.svg?react';
+import V from '@/icons/wIcon.svg?react';
+import I from '@/icons/iIcon.svg?react';
+import F from '@/icons/fIcon.svg?react';
+import N from '@/icons/nIcon.svg?react';
+import D from '@/icons/dIcon.svg?react';
+import A from '@/icons/aIcon.svg?react';
 import {
-  HeroBg,
+
   HeroPictureWrapper,
   HeroTextWrapper,
   MainHeroStyled,
-  SvgWrapper,
+  SvgBottomLine,
+  SvgTopLine,
+
 } from './MainHero.styled';
-import Container from '../Container';
-import Section from '../Section';
 
 const MainHero: FC = () => (
   <Section>
@@ -28,55 +41,85 @@ const MainHero: FC = () => (
           day, a great way to fill this gap can be compensated through a glass
           of wine.
         </p>
-        <Button
-          title='Shop now'
-          width={327}
-          height={41}
-          sidePadding={20}
-          fontSize={14}
-          type={ButtonTypes.button}
-          buttonDesign={ButtonDesign.burgundy}
-          onClick={(e) => {
-            e.currentTarget.blur();
-          }}
-        />
+        <Link to='/store'>
+          <Button
+            title='Shop now'
+            width={327}
+            height={43}
+            sidePadding={20}
+            fontSize={14}
+            type={ButtonTypes.button}
+            onClick={(e) => {
+              e.currentTarget.blur();
+            }}
+          />
+        </Link>
       </Container>
       <HeroPictureWrapper>
         <picture>
           <source
-            srcSet={`${wineTime.tab1xWebp}1x, ${wineTime.tab2xWebp}2x`}
+            srcSet={`${hero.heroDesc1xWebp} 1x, ${hero.heroDesc2xWebp} 2x`}
             type='image/webp'
-            media={'min-width: 1440px'}
+            media='(min-width: 1440px)'
           />
           <source
-            srcSet={`${wineTime.tab1xWebp}1x, ${wineTime.tab2xWebp}2x`}
+            srcSet={`${hero.heroTab1xWebp} 1x, ${hero.heroTab2xWebp} 2x`}
             type='image/webp'
-            media={'min-width: 768px'}
+            media='(min-width: 768px)'
           />
           <source
-            srcSet={`${wineTime.mob1xWebp}1x, ${wineTime.mob2xWebp}2x`}
+            srcSet={`${hero.heroMob1xWebp} 1x, ${hero.heroMob2xWebp} 2x`}
             type='image/webp'
-            media={'min-width: 375px'}
+            media='(min-width: 375px)'
           />
           <source
-            srcSet={`${wineTime.tab1xJpg}1x, ${wineTime.tab2xJpg}2x,`}
-            media={'min-width: 1440px'}
+            srcSet={`${hero.heroDesc1xJpg} 1x, ${hero.heroDesc2xJpg} 2x,`}
+            media='(min-width: 1440px)'
           />
           <source
-            srcSet={`${wineTime.tab1xJpg}1x, ${wineTime.tab2xJpg}2x,`}
-            media={'min-width: 768px'}
+            srcSet={`${hero.heroTab1xJpg} 1x, ${hero.heroTab2xJpg} 2x,`}
+            media='(min-width: 768px)'
           />
           <source
-            srcSet={`${wineTime.mob1xJpg}1x, ${wineTime.mob2xJpg}2x,`}
-            media={'min-width: 375px'}
+            srcSet={`${hero.heroMob1xJpg} 1x, ${hero.heroMob2xJpg} 2x,`}
+            media='(min-width: 375px)'
           />
-          <HeroBg src={`${wineTime.mob1xJpg}`} alt='Wine time image' />
+          <img src={`${hero.heroMob1xJpg}`} alt='Hero image' />
         </picture>
+    
+          <SvgTopLine>
+            <div className='we'>
+              <W />
+              <E />
+            </div>
+            <div className='love'>
+              <L />
+              <O />
+              <V />
+              <E />
+            </div>
+            <div className='life'>
+              <L />
+              <I />
+              <F />
+              <E />
+            </div>
+          </SvgTopLine>
+          <SvgBottomLine>
+            <div className='end'>
+              <A />
+              <N />
+              <D />
+            </div>
+            <div className='wine'>
+              <W />
+              <I />
+              <N />
+              <E />
+            </div>
+          </SvgBottomLine>
+       
       </HeroPictureWrapper>
-      <SvgWrapper>
-        <div className='firstSvgLine'></div>
-        <div className='secondSvgLine'></div>
-      </SvgWrapper>
     </MainHeroStyled>
   </Section>
 );
