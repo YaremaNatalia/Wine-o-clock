bd03a61f12930f97a3375115aaaf244dcf45e9a6
YaremaNatalia <yaremka.n@gmail.com>
2024-02-16 23:24:36 +0200

    add types

--- src/types/types.ts
+++ src/types/types.ts
@@ -25,7 +25,6 @@ export type PrivateLinks = Readonly<IPrivateLink[]>;
 
 export type BtnClickEvent = MouseEvent<HTMLButtonElement>;
 
-
 export type LinkClickEvent = MouseEvent<HTMLAnchorElement>;
 
 export interface ICredentials {
@@ -33,22 +32,76 @@ export interface ICredentials {
   password: string;
 }
 
+// export interface IWine {
+//   _id: string;
+//   name: string;
+//   photo: string;
+//   color: string;
+//   sweetness: string;
+//   country: string;
+//   region?: string;
+//   volume: string;
+//   alcohol: string;
+//   price: number;
+//   description: string;
+//   number_in_stock: number;
+//   sale?: boolean;
+//   discount?: number;
+//   [key: string]: string | number | boolean | undefined;
+// }
+
 export interface IWine {
-  _id: string;
-  name: string;
-  photo: string;
-  color: string;
-  sweetness: string;
-  country: string;
-  region?: string;
-  volume: string;
-  alcohol: string;
+  id: number;
+  wineName: string;
   price: number;
-  description: string;
-  number_in_stock: number;
-  sale?: boolean;
-  discount?: number;
-  [key: string]: string | number | boolean | undefined;
+  adminDiscountPercentage: number;
+  wineDescription: string;
+  quantity: number;
+  bottleCapacity: string;
+  alcohol: string;
+  isNewCollection: boolean;
+  isBestSeller: boolean;
+  isSale: boolean;
+  isWineTimePromotion: boolean;
+  winemaking: string;
+  grapeVarieties: string;
+  tastingNotes: string;
+  storeAndServeAdvices: string;
+  foodPairing: string;
+  reviewsAndAwards?: string;
+  wineColor: string;
+  sugarConsistency: string;
+  country: ICountry;
+  region: IRegion;
+  evaluation?: number;
+  wineComments?: IComment[];
+  bottlesSoldCounter: number;
+  addedDateTime: string;
+  imageUrl: string;
+  [key: string]:
+    | string
+    | number
+    | boolean
+    | ICountry
+    | IRegion
+    | IComment[]
+    | undefined;
 }
 
+export interface ICountry {
+  id: number;
+  name: string;
+}
 
+export interface IRegion {
+  id: number;
+  name: string;
+}
+
+export interface IComment {
+  id: number;
+  userFirstName: string;
+  userLastName: string;
+  comment: string;
+  addedCommentTime: string;
+}
