f15ea61f419d2888cc3db6a92d8b602ed52291ea
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-04-21 12:19:49 +0300

    Update Header.styled.ts

--- src/components/Header/Header.styled.ts
+++ src/components/Header/Header.styled.ts
@@ -6,7 +6,6 @@ export const StyledHeader = styled.header`
   top: 0;
   left: 0;
   z-index: 10;
-  width: 100%;
   display: inline-block;
   min-width: 100%;
   box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.25);
