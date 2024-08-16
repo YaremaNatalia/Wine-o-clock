ad06abc0b1b7c165587c13df53630b128901b121
YaremaNatalia <yaremka.n@gmail.com>
2024-08-16 21:36:13 +0300

    fix login modal

--- src/components/ModalWin/ModalWin.styled.ts
+++ src/components/ModalWin/ModalWin.styled.ts
@@ -1,25 +1,20 @@
 import styled from '@emotion/styled';
 
 export const Backdrop = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: rgba(18, 20, 23, 0.5);
-  z-index: 1000;
-  overflow: hidden;
+  background-color: rgba(18, 20, 23, 0.1);
+  padding: ${({ theme }) => `${theme.spacing(10)} ${theme.spacing(2)}`};
+  -webkit-overflow-scrolling: touch;
+  overflow-y: auto;
 `;
 
 export const Container = styled.div`
   position: relative;
-  max-width: 100vw;
-  max-height: 100vh;
-  overflow-y: auto;
-  -webkit-overflow-scrolling: touch;
   border-radius: 8px;
   background-color: ${({ theme }) => theme.colors.primaryWhite};
 `;
+
--- src/components/ModalWin/ModalWin.tsx
+++ src/components/ModalWin/ModalWin.tsx
@@ -1,15 +1,8 @@
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 import { IProps } from './ModalWin.types';
 import { Backdrop, Container } from './ModalWin.styled';
 
 const ModalWin: FC<IProps> = ({ children }) => {
-  useEffect(() => {
-    document.body.style.overflow = 'hidden';
-
-    return () => {
-      document.body.style.overflow = 'auto';
-    };
-  }, []);
   return (
     <Backdrop>
       <Container>{children}</Container>
